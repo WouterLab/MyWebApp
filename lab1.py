28baf12c14e2334a72d1c465238e11c8ee9b4e80
WouterLab <59249965+WouterLab@users.noreply.github.com>
2022-04-11 18:28:04 +0300

    Add files via upload

---
+++ lab1.py
@@ -0,0 +1,17 @@
+import math
+
+a = 1.5
+b = 3
+c = 1.5
+d = b ** 2 - 4 * a * c
+print("Дискриминант D =  %.2f"  % d)
+
+if d > 0:
+    x1 = (-b + math.sqrt(d)) / (2 * a)
+    x2 = (-b - math.sqrt(d)) / (2 * a)
+    print("x1 = %.2f \nx2 = %.2f" % (x1, x2))
+elif d == 0:
+    x = -b / (2 * a)
+    print("x = %.2f" % x)
+else:
+    print("Корней нет")
